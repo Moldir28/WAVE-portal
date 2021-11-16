c78149d74b0b77c450ca0c0ac2252d9fceda7819
Moldir28 <75260678+Moldir28@users.noreply.github.com>
2021-11-16 11:02:38 +0400

    changed the path to home page

---
+++ build/precache-manifest.d3aa9ba0691ac5d507b912697b1b2437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb83cd1f75dffd7826b88e11fc82cc93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "627683f583649e4f6e7d",
+    "url": "./static/css/main.41ec9aa5.chunk.css"
+  },
+  {
+    "revision": "8a92a113359837c67dc2",
+    "url": "./static/js/2.bf2c7c23.chunk.js"
+  },
+  {
+    "revision": "627683f583649e4f6e7d",
+    "url": "./static/js/main.5d976bd0.chunk.js"
+  },
+  {
+    "revision": "471fd3c2f4cd16231044",
+    "url": "./static/js/runtime-main.a23717fc.js"
+  }
+]);
